26a9fb68144899147e0f72b34facb57ade483d8d
manucarenjot <carenjotje@gmail.com>
2024-03-01 22:34:01 +0100

    Ajout d'un box-shadow pour donner l'aspect du boite

--- src/components/cardProfile.vue
+++ src/components/cardProfile.vue
@@ -5,7 +5,7 @@
                 <img class="content-img" src="../assets/reed-richards.png" alt="bachelor" width="100%" height="100%">
                 <h2 class="content-title">Reed Richards</h2>
             </div>
-            <div class="glow" @mousemove="cardAnime(), glowAnime()">
+            <div class="glow" @mousemove="cardAnime(), glowAnime()" @mouseleave="moveOut()">
 
             </div>
         </div>
@@ -29,36 +29,42 @@
             cardAnime(){
                 const card = document.querySelector('.card');
                 
-                const X = (event.offsetX ) / card.offsetWidth;
-                const Y = (event.offsetY) / card.offsetHeight;
-        
-                console.log((X - 0.5) * -60 +' et '+ (Y - 0.5 - Y) * -60);
+                const X = event.offsetX  / card.offsetWidth - 0.5;
+                const Y = 0.5 - event.offsetY / card.offsetHeight;
 
-                 card.style.transform = 'rotateX('+ (X - 0.5) * 60 +'deg) rotateY('+(Y - 0.5) * -60 +'deg)';
-                 
+                const rotateX = X*60;
+                const rotateY = Y*60;
+                console.log(-rotateX+ ' '+ -rotateY);
 
-      
-      
-                
-
-                 card.style.opacity = 1;
-                    
+                card.style.transform = 'rotateX('+ rotateX +'deg) rotateY('+rotateY+'deg)';
+                card.style.boxShadow = -rotateX/1.4 + 'px '+ rotateY/1.4 +'px red';
+                card.style.opacity = 1;
             },
             glowAnime(){
               
                 const glow = document.querySelector('.glow');
-                const X = (event.offsetX) / glow.offsetWidth;
-                const Y = (event.offsetY) / glow.offsetHeight;
-        
-                console.log(( X - 0.5) * -60 +' et g '+ (Y - 0.5 - Y) * -60);
+                const X = event.offsetX  / glow.offsetWidth - 0.5;
+                const Y = 0.5 - event.offsetY / glow.offsetHeight;
 
-                 glow.style.transform = 'rotateX('+ (X - 0.5)  +'deg) rotateY('+(Y - 0.5)  +'deg)';
+                let glowX = (X -0.5) * 100;
+                let glowY = (Y -0.5) * 100;
+                const rotateX = X*60;
+                const rotateY = Y*60;
+                console.log('radial-gradient(circle at'+glowX+'% '+glowY+'%, rgb(184, 196, 211), transparent)');
+                glow.style.transform = 'rotateX('+ (X - 0.5)  +'deg) rotateY('+(Y - 0.5)  +'deg)';
+                glow.style.background= 'radial-gradient(circle at '+glowY+'% '+ -glowX+'%, rgb(185, 106, 3), transparent)';
+                glow.style.boxShadow = -rotateX/1.8 + 'px '+ rotateY/1.8 +'px rgba(0,0,0,0.28)';
+            },
 
-      
-      
-                 
+            moveOut(){
+                const card = document.querySelector('.card');
+                const glow = document.querySelector('.glow');
 
-                 
+                card.style.transform = 'rotateX('+ 0 +'deg) rotateY('+0+'deg)';
+                card.style.boxShadow = '';
+                glow.style.transform = 'rotateX('+ 0  +'deg) rotateY('+ 0 +'deg)';
+                glow.style.background= 'radial-gradient(circle at '+50+'% '+ 0+'%, rgb(185, 106, 3), transparent)';
+                glow.style.boxShadow = '';
             }
             
             
@@ -87,11 +93,13 @@
 }
 .card {
     width: 20%;
-    height: 350px;
+    height: 500px;
     display: flex;
     flex-direction: column;
     justify-content: start;
     perspective: 500px;
+    border-radius: 25px;
+    
 }
 
 .content-card {
@@ -100,6 +108,7 @@
     overflow: hidden;
     border-radius: 25px;
     transition:  all 0.15s ease-out;
+    border: 3px red solid;
 }
 
 .content-img{
@@ -118,6 +127,7 @@
     margin: 0;
     text-align: center;
     padding-top: 20px;
+    
 }
 
 .glow {
@@ -129,7 +139,7 @@
     height: 100%;
     transition:  all 0.15s ease-out;
     mix-blend-mode: hard-light;
-    background: radial-gradient(circle at 50% 0%, rgb(184, 196, 211), transparent);
+    background: radial-gradient(circle at 50% 0%, rgb(185, 106, 3), transparent);
     perspective: 500px;
 }
 
